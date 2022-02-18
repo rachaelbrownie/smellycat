7765674c09f8f0e40d42642669ca6ffcca45425f
RoacheDezigne <roachedezigne@gmail.com>
2022-02-18 17:27:09 -0500

    path improvements
    remembering that is was actually super simple…

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import { getCoursesByUser } from "./api/getCoursesByUser";
-import { CourseContent } from './components/CourseContent';
-import { Course } from './components/Course';
-import { FavoritesButton } from './components/FavoritesButton';
+import {
+  CourseContent,
+  Course,
+  FavoritesButton,
+} from "./components";
 
 function App() {
   const [courses, setCourses] = useState<any>([]);
@@ -20,28 +22,28 @@ function App() {
   const DisplayCourse = ({ data }: any | null) => {
     if ((isFavsOnly && data.favorite) || !isFavsOnly) {
       return (
-         <Course 
-          title={data.title} 
-          instructor={data.instructor_name} 
-          image={data.instructor_image_url} 
+        <Course
+          title={data.title}
+          instructor={data.instructor_name}
+          image={data.instructor_image_url}
           favorite={data.favorite}
           key={data.id}
           id={data.id}
         />
-      )
+      );
     }
 
     return null;
-  }
+  };
 
   return (
     <div className="App">
       <header className="App-header"></header>
       <FavoritesButton onClick={() => setIsFavsOnly(!isFavsOnly)} />
       <CourseContent>
-        {courses.map((data: any) => 
+        {courses.map((data: any) => (
           <DisplayCourse data={data} key={`display${data.id}`} />
-        )}
+        ))}
       </CourseContent>
       <footer></footer>
     </div>
---
+++ src/components/index.tsx
@@ -0,0 +1,5 @@
+export * from './Course';
+export * from './CourseContent';
+export * from './FavoritesButton';
+export * from './Grid';
+export * from './Heart';
